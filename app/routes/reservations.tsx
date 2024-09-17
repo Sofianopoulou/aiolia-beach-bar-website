a460fe8ce775c4928e0a7a9f1015b7da5bcc4d0a
Apostolia Maria Sofianopoulou <sofliamariah1d@gmail.com>
2024-09-17 16:40:45 +0300

    fix: remove if env variable existance from reservations.tsx

--- app/routes/reservations.tsx
+++ app/routes/reservations.tsx
@@ -2,10 +2,6 @@ import { json, redirect } from "@remix-run/node";
 import { Resend } from "resend";
 import { useActionData } from "@remix-run/react";
 
-if (!process.env.RESEND_API_KEY) {
-  throw new Error("RESEND_API_KEY is not set.");
-}
-
 const resend = new Resend(process.env.RESEND_API_KEY);
 
 type ActionData = {
