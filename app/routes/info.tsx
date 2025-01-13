f3149e5825209dc805b64fc2d8ef8771390d62f3
Apostolia Maria Sofianopoulou <sofliamariah1d@gmail.com>
2025-01-13 18:46:09 +0000

    Add translation to info

--- app/routes/info.tsx
+++ app/routes/info.tsx
@@ -11,15 +11,10 @@ export default function Info() {
           {t("Welcome to Aiolia Beach Bar")}
         </h3>
         <p className="text-base leading-7 text-gray-600 mb-4 font-sans">
-          At Aiolia, we curate an unparalleled dining experience, offering
-          mouthwatering delights that captivate your taste buds. Immerse
-          yourself in the heart of seaside bliss as you savor our signature
-          cocktails, gourmet pizzas, crisp salads, and aromatic coffee. Our
-          commitment to consistent quality ensures every visit is a culinary
-          journey. Unwind, relish delicious fare, and let the beach vibes
-          elevate your moments. Aiolia, your ultimate destination in Nea
-          Anchialos and Volos. Cheers to shared memories and flavors that
-          linger! 🏖️🍹
+          {t(
+            "At Aiolia, we curate an unparalleled dining experience, offering mouthwatering delights that captivate your taste buds. Immerse yourself in the heart of seaside bliss as you savor our signature cocktails, gourmet pizzas, crisp salads, and aromatic coffee. Our commitment to consistent quality ensures every visit is a culinary journey. Unwind, relish delicious fare, and let the beach vibes elevate your moments. Aiolia, your ultimate destination in Nea Anchialos and Volos. Cheers to shared memories and flavors that linger!"
+          )}{" "}
+          🏖️🍹
         </p>
       </div>
       <div className="p-6 bg-white rounded-lg shadow-lg max-w-[90%] mx-auto text-center border border-gray-200">
